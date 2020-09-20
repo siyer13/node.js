599a7e36df3fe01687f6eb11f57c42a358b5dba8
Sridhar <sreedharriyer@gmail.com>
2020-09-20 22:00:11 +0530

    Update server.test.js

--- countries-app/test/server.test.js
+++ countries-app/test/server.test.js
@@ -1,8 +1,18 @@
 const assert = require('assert');
-
 let chai = require('chai');
 let chaiHttp = require('chai-http');
-let server = require('../server');
+let server = require('../src/server');
 let should = chai.should();
-
+//module.exports = server.listen(3000);
 chai.use(chaiHttp);
+
+it("Capital should be Washington, D.C.", (done) => {
+       chai.request('http://localhost:3000')
+            .post("/")
+            .send({code: "IN"})
+            .end((err, res) => {
+                res.should.have.status(200);
+                console.log("Response Body:", res.body);
+            })
+    done()
+})
